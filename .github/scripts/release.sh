26fd30579bd739b0eb508e0dd7e22ed593e901ce
Sibin Grasic <sibin.grasic@oblak.studio>
2023-08-01 14:40:13 +0200

    feat: Implemented provenance

--- .github/scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NEXT_VERSION=$1
-CURRENT_VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[:space:]')
-
-sed -i "s/\"version\": \"$CURRENT_VERSION\"/\"version\": \"$NEXT_VERSION\"/g" package.json
-
-zip -r /tmp/release.zip ./dist ./package.json
